1fe452d89cee4881b0487538dc42277ba9b51884
AppVeyor <ci@appveyor.com>
2023-09-20 20:39:21 +0000

    b7b2b4e

---
+++ main.js
@@ -0,0 +1,22 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		__webpack_require__.p = "";
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/img/goblin.png
+const goblin_namespaceObject = __webpack_require__.p + "2dbd01ce16c0fa83cb67.png";
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+/******/ })()
+;
